d76d1c3ee70ee061757a137481e59664b6c7056e
Johan Walberg <johan@kreditz.com>
2019-02-26 10:32:29 +0100

    installed bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= jquery_ujs
+//= require bootstrap-sprockets
+//= jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
