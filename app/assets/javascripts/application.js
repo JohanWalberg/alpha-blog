f467521d335cf8aac3953f1d2e8de93d12e9c248
Johan Walberg <johan@kreditz.com>
2019-02-26 11:44:39 +0100

    Create homepage and add links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-//= jquery_ujs
+//= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
-//= jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
